40c3f031206e6bb1ff2da6a1ebacc08f4fc630f0
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-05-10 14:08:50 +0200

    BUMP

--- components/Marker/Box.vue
+++ components/Marker/Box.vue
@@ -4,16 +4,35 @@ const wrapper = ref<HTMLElement>()
 
 <template>
   <div ref="wrapper" class="markerbox" data-from>
-    <slot />
+    <div class="content"><slot /></div>
+    <div class="layer" />
   </div>
 </template>
 
 <style scoped>
 .markerbox {
   position: relative;
-  padding: var(--space-s);
   border-radius: var(--radius);
   background: var(--background);
   width: 250px;
+}
+
+.content {
+  position: relative;
+  padding: var(--space-s);
+  top: 0; left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+
+.layer {
+  background: var(--background);
+  position: absolute;
+  top: 0; left: 0;
+  border-radius: var(--radius);
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 }
 </style>
