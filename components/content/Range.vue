9b48f6479ea7c8941ef7607f362167f5c9405f14
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-05-13 00:35:46 +0200

    changes theme and adds range

---
+++ components/content/Range.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="range">
+      <div class="foreground">
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+      </div>
+      <div class="background">
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  gap: var(--space-xl);
+  border-radius: var(--radius);
+  padding: 1rem;
+  background: var(--background-10);
+  margin-bottom: var(--space-xl);
+}
+</style>
