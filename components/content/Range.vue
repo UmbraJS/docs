3d32857dbd5314f80896bad4866451eb16ec1394
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-05-13 02:04:38 +0200

    adds range

--- components/content/Range.vue
+++ components/content/Range.vue
@@ -4,15 +4,15 @@
 <template>
   <div class="wrapper">
     <div class="range">
-      <div class="foreground">
-        <div class="square"></div>
-        <div class="square"></div>
-        <div class="square"></div>
-      </div>
       <div class="background">
-        <div class="square"></div>
-        <div class="square"></div>
-        <div class="square"></div>
+        <div class="square" style="background: var(--background)"></div>
+        <div class="square" style="background: var(--background-10)"></div>
+        <div class="square" style="background: var(--background-20)"></div>
+      </div>
+      <div class="foreground">
+        <div class="square" style="background: var(--foreground-20)"></div>
+        <div class="square" style="background: var(--foreground-10)"></div>
+        <div class="square" style="background: var(--foreground)"></div>
       </div>
     </div>
   </div>
@@ -27,5 +27,18 @@
   padding: 1rem;
   background: var(--background-10);
   margin-bottom: var(--space-xl);
+}
+
+.range {
+  border-radius: var(--radius);
+  overflow: hidden;
+}
+
+.range .square {
+  width: 30px;
+  height: 30px;
+  background-color: var(--accent);
+  display: flex;
+  justify-content: center;
 }
 </style>
