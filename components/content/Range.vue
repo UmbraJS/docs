ba2f72162900b61b3aebeefc8c7e2f5a7674dae0
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-05-16 20:54:02 +0200

    adds back range

--- components/content/Range.vue
+++ components/content/Range.vue
@@ -5,20 +5,26 @@
   <div class="wrapper">
     <div class="range">
       <div class="background">
-        <div class="square" style="background: var(--background)"></div>
-        <div class="square" style="background: var(--background-10)"></div>
-        <div class="square" style="background: var(--background-20)"></div>
+        <div class="square" style="background: var(--background)" />
+        <p>- background</p>
+        <div class="square" style="background: var(--background-10)" />
+        <p>- background-10</p>
+        <div class="square" style="background: var(--background-20)" />
+        <p>- background-20</p>
       </div>
       <div class="foreground">
-        <div class="square" style="background: var(--foreground-20)"></div>
-        <div class="square" style="background: var(--foreground-10)"></div>
-        <div class="square" style="background: var(--foreground)"></div>
+        <div class="square" style="background: var(--foreground-20)" />
+        <p>- foreground-20</p>
+        <div class="square" style="background: var(--foreground-10)" />
+        <p>- foreground-10</p>
+        <div class="square" style="background: var(--foreground)" />
+        <p>- foreground</p>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .wrapper {
   display: flex;
   justify-content: center;
@@ -30,8 +36,33 @@
 }
 
 .range {
-  border-radius: var(--radius);
+  display: grid;
   overflow: hidden;
+}
+
+.range .background,
+.range .foreground {
+  display: grid;
+  column-gap: var(--space-s);
+  grid-template-columns: auto 1fr;
+  p {
+    color: var(--foreground-20) !important;
+  }
+}
+
+.range .background p:nth-of-type(1),
+.range .foreground p:nth-last-of-type(1) {
+  color: var(--foreground) !important;
+}
+
+.range .background .square:nth-of-type(1) {
+  border-top-right-radius: var(--radius);
+  border-top-left-radius: var(--radius);
+}
+
+.range .foreground .square:nth-last-of-type(1) {
+  border-bottom-right-radius: var(--radius);
+  border-bottom-left-radius: var(--radius);
 }
 
 .range .square {
